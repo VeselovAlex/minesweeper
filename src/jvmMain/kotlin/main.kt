080e652830aec07e72f00ee72118ad1c6ee39d81
Aleksandr Veselov <veselov143@gmail.com>
2021-09-25 22:02:33 +0300

    MOve icon to shared resources, adjust cell size for desktop

--- src/jvmMain/kotlin/main.kt
+++ src/jvmMain/kotlin/main.kt
@@ -2,6 +2,7 @@ package com.github.veselovalex.minesweeper
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -9,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -36,9 +38,8 @@ actual fun CellView(cell: Cell) {
     val openedCellColor = Color.White
     val color = if (cell.isOpened) { openedCellColor } else { closedCellColor }
 
-
     return Box(
-        modifier = org.jetbrains.compose.common.ui.Modifier.size(32.dp, 32.dp)
+        modifier = org.jetbrains.compose.common.ui.Modifier.size(40.dp, 40.dp)
             .background(color)
             .border(1.dp, Color(0xDD, 0xDD, 0xDD))
             .clickable { cell.open() }
@@ -46,16 +47,16 @@ actual fun CellView(cell: Cell) {
         if (cell.isOpened) {
             if (cell.hasBomb) {
                 Image(
-                    painter = painterResource("mine.png"),
+                    painter = painterResource("assets/mine.png"),
                     contentDescription = "Bomb",
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize().padding(all = Dp(4.0f))
                 )
             } else if (cell.bombsNear > 0) {
                 Text(
                     text = cell.bombsNear.toString(),
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.Bold,
-                    fontSize = 24.sp,
+                    fontSize = 32.sp,
                     modifier = Modifier.fillMaxSize()
                 )
             }
