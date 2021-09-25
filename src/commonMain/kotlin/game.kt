b7c87c016d7256aa3f9fe3bbfe5b00ac1ef7b45a
Aleksandr Veselov <veselov143@gmail.com>
2021-09-25 19:09:51 +0300

    Draw empty board

--- src/commonMain/kotlin/game.kt
+++ src/commonMain/kotlin/game.kt
@@ -1,7 +1,32 @@
 package com.github.veselovalex.minesweeper.common
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.common.material.Text
+import org.jetbrains.compose.common.core.graphics.Color
+import org.jetbrains.compose.common.foundation.border
+import org.jetbrains.compose.common.foundation.layout.*
+import org.jetbrains.compose.common.ui.Modifier
+import org.jetbrains.compose.common.ui.background
+import org.jetbrains.compose.common.ui.size
+import org.jetbrains.compose.common.ui.unit.dp
 
 @Composable
-fun Game() = Text("It Works")
+fun Game() = Column(Modifier.fillMaxWidth()) {
+    val rows = 8
+    val columns = 8
+
+    Column {
+        for (row in 1..rows) {
+            Row {
+                for (column in 1..columns) {
+                    Box(
+                        modifier = Modifier.size(32.dp, 32.dp)
+                            .background(Color.DarkGray)
+                            .border(1.dp, Color.White)
+                    ) {
+                        // No content
+                    }
+                }
+            }
+        }
+    }
+}
